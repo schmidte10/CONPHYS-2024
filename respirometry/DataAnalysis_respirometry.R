c2dbe8d5a2f65ab7b3dd65f2a940b2fd1b89b0ce
schmidte10 <elliott.a.schmidt@gmail.com>
2022-10-01 05:43:09 +1000

    imported data and made minor corrects once imported

--- respirometry/DataAnalysis_respirometry.R
+++ respirometry/DataAnalysis_respirometry.R
@@ -1,8 +1,17 @@
 #--- load libraries ---#  
 library(tidyverse) 
+library(plyr)
+library(dplyr)
 
 #--- set working directory ---#
 setwd("C:/Users/Elliott/OneDrive - James Cook University/PhD dissertation/Data/Local_adaptation/Chapter1_LocalAdaptation")
 
 #--- load data ---# 
 resp <- read.delim("./respirometry/SummaryData_2022_resp.txt")
+
+# data seems to have loaded with two extract columns at the end 
+# remove extract columns by name 
+
+resp <- resp %>% select(-c("X","X.1"))
+
+#
