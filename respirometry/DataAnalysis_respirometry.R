d22faed6b673628b551bd3d6666c777eb895101f
schmidte10 <elliott.a.schmidt@gmail.com>
2022-10-01 05:28:00 +1000

    uploading initial files

---
+++ respirometry/DataAnalysis_respirometry.R
@@ -0,0 +1,8 @@
+#--- load libraries ---#  
+library(tidyverse) 
+
+#--- set working directory ---#
+setwd("C:/Users/Elliott/OneDrive - James Cook University/PhD dissertation/Data/Local_adaptation/Chapter1_LocalAdaptation")
+
+#--- load data ---# 
+resp <- read.delim("./respirometry/SummaryData_2022_resp.txt")
