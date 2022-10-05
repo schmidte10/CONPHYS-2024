83612fa77f66f23aeaaf3a501ff2a1c106dc3c09
schmidte10 <elliott.a.schmidt@gmail.com>
2022-10-06 05:59:39 +1100

    working directory set twice once to get to file to load data and then again to reset directory to RMR file for resting data

--- respirometry/RMR/DataAnalysis_respirometry.R
+++ respirometry/RMR/DataAnalysis_respirometry.R
@@ -18,10 +18,11 @@ library(broom)
 library(emmeans)
 library(ggeffects)
 #--- set working directory ---#
-setwd("C:/Users/Elliott/OneDrive - James Cook University/PhD dissertation/Data/Local_adaptation/Chapter1_LocalAdaptation")
+setwd("C:/Users/Elliott/OneDrive - James Cook University/PhD dissertation/Data/Local_adaptation/Chapter1_LocalAdaptation/respirometry/")
 
 #--- load data ---# 
-resp <- read.delim("./respirometry/SummaryData_2022_resp.txt")
+resp <- read.delim("./SummaryData_2022_resp.txt")
+setwd("C:/Users/Elliott/OneDrive - James Cook University/PhD dissertation/Data/Local_adaptation/Chapter1_LocalAdaptation/respirometry/RMR/")
 
 # data seems to have loaded with two extract columns at the end 
 # remove extract columns by name 
