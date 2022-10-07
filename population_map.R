97bc3aa431c10bb14acb9acc10f114d69bd81532
schmidte10 <elliott.a.schmidt@gmail.com>
2022-10-07 13:17:19 +1100

    finished final map showing were fish populations were sourced from

--- population_map.R
+++ population_map.R
@@ -6,6 +6,8 @@ library(rnaturalearthdata)
 library(ggspatial)
 library(tidyverse)
 library(cowplot)
+library(grid)
+library(ggpubr)
 #--- Loading shape data ---#
 GBR_data <- st_read("C:/Users/Elliott/OneDrive - James Cook University/PhD dissertation/Statistics_wrkshp/Maps/TS_AIMS_NESP_Torres_Strait_Features_V1b_with_GBR_Features")
 class(GBR_data)
@@ -93,33 +95,49 @@ p1 <- ggplot() +
   annotate("text", x = 149.8, y = -14, label = "Coral \nSea", fontface = "italic", size = 6);p1 
 
 insert1 <- ggplot() +
-  geom_sf(data = coords,fill = "darkblue", color = "darkblue", size = 0.5) +
+  geom_sf(data = coords,fill = "gray90", color = "darkblue", size = 0.5) +
   geom_sf(data = AUS,fill = "gray90", color = "#333333", size = 0.1) +
-  annotation_scale(location = "bl", width_hint = 0.5) +
-  annotation_north_arrow(location = "bl", which_north = "true", 
-                         pad_x = unit(0.1, "in"), pad_y = unit(0.25, "in"),
-                         style = north_arrow_fancy_orienteering) +
-  coord_sf(xlim = c(145, 147), ylim = c(-17.6, -16), expand = F) + 
-  xlab("Longitude") + ylab("Latitude") +
-  theme(panel.background = element_rect(fill = "lightblue")) + 
-  annotate("segment", x = 145.7926, xend = 145.7926+0.3, y = -16.32093, yend = -16.32093+0.28, colour = "black", size = 1)+
-  annotate("segment", x = 146.2049, xend = 146.2049+0.3, y = -16.99838, yend = -16.99838+0.28, colour = "black", size = 1)+
-  annotate("segment", x = 145.9929, xend = 145.9929+0.3, y = -16.65520, yend = -16.65520+0.28, colour = "black", size = 1)+
-  geom_point(data = myreefs.core, aes(x = X_COORD, y = Y_COORD), size = 6, 
-             shape = 21, fill = "#4c8494") + 
-  geom_text(data = myreefs.core, aes(X_COORD+.3, Y_COORD+.3, label=QLD_NAME)); insert1
+  annotation_scale(location = "bl", width_hint = 0.3) +
+  coord_sf(xlim = c(145, 147.5), ylim = c(-17.6, -16), expand = F) +
+  theme(panel.background = element_rect(fill = "lightblue")) +
+  xlab("")+ylab("")+
+  annotate("segment", x = 145.7926, xend = 145.7926+0.3, y = -16.32093, yend = -16.32093+0.18, colour = "black", size = 1)+
+  annotate("segment", x = 146.2049, xend = 146.2049+0.3, y = -16.99838, yend = -16.99838+0.18, colour = "black", size = 1)+
+  annotate("segment", x = 145.9929, xend = 145.9929+0.3, y = -16.65520, yend = -16.65520+0.18, colour = "black", size = 1)+
+  geom_point(data = myreefs.core, aes(x = X_COORD, y = Y_COORD), size = 5, 
+             shape = 21, fill = "#2f3544") + 
+  geom_label(data = myreefs.core, aes(X_COORD+.3, Y_COORD+.2, label=QLD_NAME), fill = "white")+
+  theme(axis.text.x=element_blank(), 
+        axis.ticks.x=element_blank(), 
+        axis.text.y=element_blank(),
+        panel.border = element_rect(colour = "#2f3544", fill=NA, size=2),
+        axis.ticks.y=element_blank()); insert1
 
 insert2 <- ggplot() +
   geom_sf(data = coords,fill = "gray90", color = "darkblue", size = 0.5) +
   geom_sf(data = AUS,fill = "gray90", color = "#333333", size = 0.1) +
-  annotation_scale(location = "bl", width_hint = 0.5) +
-  annotation_north_arrow(location = "bl", which_north = "true", 
-                         pad_x = unit(0.1, "in"), pad_y = unit(0.25, "in"),
-                         style = north_arrow_fancy_orienteering) +
-  coord_sf(xlim = c(148.5, 151), ylim = c(-21.7, -20.2), expand = F) + 
-  xlab("Longitude") + ylab("Latitude") +
+  annotation_scale(location = "bl", width_hint = 0.3) +
+  coord_sf(xlim = c(148.5, 151), ylim = c(-21.7, -20.2), expand = F) +
   theme(panel.background = element_rect(fill = "lightblue"), 
         panel.grid.major = element_line(colour = "lightblue")) +  
-  geom_point(data = myreefs.leading, aes(x = X_COORD, y = Y_COORD), size = 3, 
-             shape = 21, fill = "#2f3544")+ 
-  geom_text(data = myreefs.leading, aes(X_COORD, Y_COORD-0.03, label=GBR_NAME)); insert2
+  geom_point(data = myreefs.leading, aes(x = X_COORD, y = Y_COORD), size = 5, 
+             shape = 21, fill = "#4c8494")+ 
+  xlab("") + ylab("") +
+  geom_label_repel(data = myreefs.leading, aes(X_COORD, Y_COORD, label=GBR_NAME), fill = "white")+
+  theme(axis.text.x=element_blank(), 
+        axis.ticks.x=element_blank(), 
+        axis.text.y=element_blank(),
+        panel.border = element_rect(colour = "#4c8494", fill=NA, size=2),
+        axis.ticks.y=element_blank())
+insert2
+
+#---final figure ---# 
+
+cb <- ggarrange(insert1, insert2,
+          ncol = 1, nrow = 2, labels = c("B","C"), 
+          align = "hv",
+          heights = c (1,1), 
+          widths = c (1,1)); cb 
+
+map <- ggarrange(p1, cb, 
+                 ncol=2, nrow=1, labels = c("A","")); map
