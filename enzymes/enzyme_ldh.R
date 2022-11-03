75cdc851084dc90731bf8c5c971852d6b111da05
schmidte10 <elliott.a.schmidt@gmail.com>
2022-11-03 13:44:36 +1000

    enzyme analysis - model run but data is currently not normal.

--- enzymes/enzyme_ldh.R
+++ enzymes/enzyme_ldh.R
@@ -4,7 +4,13 @@ library(tidyverse)
 library(chron) 
 library(lubridate)
 library(janitor) 
-library(ggpubr)
+library(ggpubr) 
+library(MuMIn)
+library(glmmTMB)
+library(performance)
+library(DHARMa)
+library(emmeans) 
+library(ggeffects)
 #--- set working directory ---# 
 # uni computer
 setwd("C:/Users/jc527762/OneDrive - James Cook University/PhD dissertation/Data/Local_adaptation/Chapter1_LocalAdaptation/enzymes") 
@@ -58,7 +64,7 @@ for (i in sample_names_list) {
 }
 
 
-#--- making table ---# 
+#--- making table - quick processing for app---# 
 tables <- list()
 
 for (i in sample_names_list) {
@@ -109,7 +115,7 @@ tables[["white.muscle_LCKM163_40"]]
 
 
 
-#--- Template ---#
+#--- Template - use for data analysis---#
 LDH_activity <- ldh3 %>% 
   group_by(UNIQUE_SAMPLE_ID, CUVETTE) %>% 
   do({
@@ -151,5 +157,73 @@ ldh.data <- final_table %>%
          Background = as.numeric(Background),
          LDH_ACTIVITY = Mean - Background) %>% 
   drop_na() %>% 
-  inner_join(select(ldh3, c(UNIQUE_SAMPLE_ID, REGION, POPULATION)), by ="UNIQUE_SAMPLE_ID") %>% 
-  distinct(UNIQUE_SAMPLE_ID, REGION, POPULATION, .keep_all = TRUE)
+  inner_join(select(ldh3, c(UNIQUE_SAMPLE_ID, REGION, POPULATION, temperature, fish_id)), by ="UNIQUE_SAMPLE_ID") %>% 
+  distinct(UNIQUE_SAMPLE_ID, REGION, POPULATION, .keep_all = TRUE) %>% 
+  mutate(LDH_ACTIVITY_POSITIVE = LDH_ACTIVITY * -1, 
+         temperature = factor(temperature))
+
+#--- begin data analysis ---# 
+hist(ldh.data$LDH_ACTIVITY); hist(scale(ldh.data$LDH_ACTIVITY, center=TRUE, scale=FALSE)*-1) 
+# data is not normal - perhaps more samples will end up helping 
+
+ldh.data %>% 
+  group_by(fish_id) %>% 
+  summarise(sample_size = n(), 
+            Min. = min(LDH_ACTIVITY), 
+            Max. = max(LDH_ACTIVITY), 
+            Mean = mean(LDH_ACTIVITY)) 
+
+#--- models ---# 
+
+ldh.model.1 <- glmmTMB(LDH_ACTIVITY ~ 1 + REGION*temperature + (1|fish_id), 
+                       family=gaussian(), 
+                       data = ldh.data, 
+                       REML = TRUE) 
+
+
+ldh.model.2 <- glmmTMB(LDH_ACTIVITY_POSITIVE ~ 1 + REGION*temperature + (1|fish_id), 
+                       family=Gamma('inverse'), 
+                       data = ldh.data, 
+                       REML = TRUE)  
+
+ldh.model.3 <- glmmTMB(LDH_ACTIVITY_POSITIVE ~ 1 + REGION*temperature + (1|fish_id) + (1|POPULATION), 
+                       family=Gamma(), 
+                       data = ldh.data, 
+                       REML = TRUE) 
+
+ldh.model.1 %>% check_model()
+ldh.model.1 %>% simulateResiduals(plot = TRUE, integerResponse = TRUE) 
+ldh.model.1 %>% testResiduals()
+
+#--- partial plots ---# 
+ldh.model.1 %>% ggemmeans(~temperature*REGION) %>% plot()
+ldh.model.1 %>% summary()
+ldh.model.1 %>% confint()
+ldh.model.1 %>% performance::r2_nakagawa()
+
+#--- results ---# 
+ldh.model.1 %>% emmeans(~ temperature*REGION, type = "response") %>% pairs(by = "temperature") %>% summary(infer = TRUE) 
+
+#--- plot ---# 
+newdata <- ldh.model.1 %>% ggemmeans(~temperature|REGION) %>% 
+  as.data.frame() %>% 
+  rename(TEMPERATURE = x)
+  
+g1 <- ggplot(newdata, aes(y=predicted, x=TEMPERATURE, color = group)) + 
+  geom_point() + 
+  theme_classic(); g1
+
+obs <- ldh.model.1 %>% 
+  mutate(Pred = predict(ldh.model.1, re.form=NA), 
+         Resid = residuals(ldh.model.1, type = 'response'), 
+         Fit = Pred - Resid)
+
+g2 <- ggplot(newdata, aes(y=predicted, x=TEMPERATURE, color = group)) + 
+  geom_pointrange(aes(ymin=conf.low, 
+                      ymax=conf.high), 
+                  shape=19, 
+                  size=1, 
+                  position = position_dodge(0.2)) + 
+  #scale_y_continuous(limits = c(0,0.9), breaks = seq(0, 0.9, by =0.15)) + 
+  theme_classic() + ylab("PHA Swelling response (mm)") + 
+  scale_color_manual(values=c("#DA3A36","#0D47A1"), name = "Regions"); g2
